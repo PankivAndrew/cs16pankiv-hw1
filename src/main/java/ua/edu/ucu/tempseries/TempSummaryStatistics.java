3734d68102f112188b388b42942cf30acb08575b
Pankiv <pankiv_a@ucu.edu.ua>
2016-10-11 16:10:02 +0300

    1

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,10 +1,6 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    private double avgTemp;
-    private double devTemp;
-    private double minTemp;
-    private double maxTemp;
     TempSummaryStatistics(double avgTemp,double devTemp,double minTemp,double maxTemp){
     }
     
